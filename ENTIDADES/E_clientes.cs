1be12486640d8efcde7c214d7657c6b830d52876
yona16-u <84141272+yona16-u@users.noreply.github.com>
2024-03-04 05:14:08 -0500

    Add files via upload

---
+++ ENTIDADES/E_clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retoCRUD.ENTIDADES
+{
+    public class E_clientes
+    {//DEFINICION DE ENTIDADES 
+        public int IdCliente { get; set; }
+        public decimal Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public decimal Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int CodGrupo { get; set; }
+        public int CodDoc { get; set; }
+    }
+}
